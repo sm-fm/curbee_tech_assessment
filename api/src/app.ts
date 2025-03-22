e128b84e3733180b80b8025ce2a330af8f6ea486
Sean McDaniel <sm.fm.static@gmail.com>
2025-03-22 14:15:36 -0400

    Setup bones of api.

---
+++ api/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import appointmentsRouter from "./controllers/appointments";
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/appointments", appointmentsRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "API is running" });
+});
+
+export default app;
